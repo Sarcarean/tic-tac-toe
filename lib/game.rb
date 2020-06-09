96730eca2d797ad5b732e0dbaf89b2c5d9b0c297
R. Harris <43601394+Sarcarean@users.noreply.github.com>
2020-06-09 09:44:37 -0700

    Additional formatting

--- lib/game.rb
+++ lib/game.rb
@@ -3,6 +3,7 @@
 require_relative 'board'
 
 class Game
+
   #Create a new board or a blank game
   def initialize(n_board = nil)
     @my_board = n_board
@@ -17,12 +18,13 @@ class Game
   def over?
 	if not @my_board==nil and @my_board.full?
 	  return true
-	end
+	else
+	  return false
+	end	
   end
 
   #Returns if this game is a tie
   def tie?
-   puts "test 1"
 	if @my_board.full?
 	  return true
 	else
@@ -30,7 +32,7 @@ class Game
 	end
   end
 
-  #Returns the winner (if there is one)
+  #Returns the winner (if there is one) or nil
   def winner
     result = @my_board.to_s
     if result["x|x|x"]
